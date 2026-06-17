666c956dc21feadffaf2979a45a7ba468a3ae03d
Ajit Kadam <ajitkadam.100@gmail.com>
2026-06-17 15:53:34 +0530

    fix layout with google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import Script from "next/script";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,8 +20,6 @@ export const metadata: Metadata = {
     google: "4O1ZPUBdWA1m3cXyajJmdviIb92DSi44HF5yDi7UZf4"
   }
 };
-
-import Script from "next/script";
 
 export default function RootLayout({
   children,
